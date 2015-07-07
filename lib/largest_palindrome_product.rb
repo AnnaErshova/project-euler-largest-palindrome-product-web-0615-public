bd1e6978e83540c216671566804d94894201ace1
AnnaErshova <anna.ershova@gmail.com>
2015-07-07 10:11:06 -0400

    the test doesn't really check for the oo nature of the solution

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,19 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+  #candidate_array=*(10*10..99*99)
+  candidate_array = Array.new
+  (100..999).each do |x|
+    (100..999).each do |y|
+      candidate_array.push(x*y)
+    end
+  end
+  candidate_array.select{|i| i.to_s == i.to_s.reverse}.max
+end
+
+# the result will be between 10,000 and 998,001
+# 906609
+# 997799 -- prime number
+
+
+
+
+
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,13 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def answer
+    candidate_array = Array.new
+    (100..999).each do |x|
+      (100..999).each do |y|
+        candidate_array.push(x*y)
+      end
+    end
+    candidate_array.select{|i| i.to_s == i.to_s.reverse}.max
+  end
+
+end # end class
